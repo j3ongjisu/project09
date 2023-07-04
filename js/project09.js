963a4ff6462d681144309be700be420cb77ce44d
j3ongjisu <132436950+j3ongjisu@users.noreply.github.com>
2023-07-04 18:13:28 +0900

    Add files via upload

--- js/project09.js
+++ js/project09.js
@@ -56,4 +56,68 @@ $(function () {
       .siblings().removeClass('on');
   });
 
+
+
+  // .main_event에 화살표 버튼 눌러서 슬라이드 돌아가게 하기
+  $('.main_event .arrows .left').on('click', function () {
+    $('.event_slide').slick('slickPrev');
+  });
+  $('.main_event .arrows .right').on('click', function () {
+    $('.event_slide').slick('slickNext');
+  });
+
+
+  $('.event_slide').slick({
+    slidesToShow: 3,
+    arrows: false,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
+  });
+
+
+  // .main_issue에 화살표 버튼 눌러서 슬라이드 돌아가게 하기
+  $('.main_issue .arrows .left').on('click', function () {
+    $('.issue_slide').slick('slickPrev');
+  });
+  $('.main_issue .arrows .right').on('click', function () {
+    $('.issue_slide').slick('slickNext');
+  });
+
+
+  $('.issue_slide').slick({
+    slidesToShow: 5,
+    arrows: false,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
+  });
+
+
+  //to_top
+  //to_top 스크롤 위로 올리기
+  $('.to_top').on('click', function () {
+    $('html, body').animate({ scrollTop: 0 }, 600)
+  });
+
+  // to_top 스크롤 숨겼다가 나타내기
+  $(window).on('scroll', function () {
+    let sct = $(window).scrollTop();
+    // sct > 1000 ? $('.to_top').fadeIn(1000) : $('.to_top').fadeOut();
+    sct > 400 ? $('.to_top').addClass('on') : $('.to_top').removeClass('on');
+  });
+
+
 });
